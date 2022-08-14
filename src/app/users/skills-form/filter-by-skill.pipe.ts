f3e2d2dbf0702e8ba235083731bd83ff15cdd436
Ana Sofia <sofiamelod@gmail.com>
2022-08-14 16:48:08 -0300

    trocando o import no filter-by-skill (pipe)

--- src/app/users/skills-form/filter-by-skill.pipe.ts
+++ src/app/users/skills-form/filter-by-skill.pipe.ts
@@ -1,25 +1,17 @@
-import { Pipe, PipeTransform } from '@angular/core'
-import { Skills } from '../skills/skills';
+import { Pipe, PipeTransform } from "@angular/core";
+import { Skills } from "../skills/Skills";
 
-@Pipe({ name: 'filterBySkill'})
-
+@Pipe({ name: "filterBySkill" })
 export class FilterBySkill implements PipeTransform {
+  transform(skills: Skills[], skillQuery: string) {
+    skillQuery = skillQuery.trim().toLowerCase();
 
-    transform(skills: Skills[], skillQuery: string) {
-        skillQuery = skillQuery
-        .trim()
-        .toLowerCase()
-
-        if(skillQuery){
-            return skills.filter(skill => skill.habilidade
-                .toLowerCase()
-                .includes(skillQuery)
-            );
-        } else {
-            return skills
-        }
+    if (skillQuery) {
+      return skills.filter((skill) =>
+        skill.habilidade.toLowerCase().includes(skillQuery)
+      );
+    } else {
+      return skills;
     }
-
+  }
 }
-
-
